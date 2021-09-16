77e7cc4b40b1b6c3b094ee7404f06f5d7ec3b087
raloliver <contato@raloliver.com.br>
2021-09-16 05:19:16 +0100

    Adding style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
  * File: Header.js
  * Project: recnotes
  * Created: Thursday, September 9th 2021, 6:24:42 am
- * Last Modified: Thursday, September 9th 2021, 6:33:47 am
+ * Last Modified: Thursday, September 16th 2021, 5:18:37 am
  * Copyright © 2021 AMDE Agência
  */
 
@@ -10,9 +10,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <header className="app-header">
+      <h1>Notes List</h1>
+    </header>
   );
 };
 
--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -2,18 +2,31 @@
  * File: NotePage.js
  * Project: recnotes
  * Created: Thursday, September 9th 2021, 6:55:00 am
- * Last Modified: Thursday, September 9th 2021, 7:29:43 am
+ * Last Modified: Thursday, September 16th 2021, 5:17:08 am
  * Copyright © 2021 AMDE Agência
  */
 
 import React from 'react';
+import {Link} from 'react-router-dom';
+import {ReactComponent as ArrowLeft} from '../assets/arrow-left.svg';
 import notes from '../assets/data';
 
 const NotePage = ({match}) => {
   const noteId = Number(match.params.id);
   const note = notes.find((note) => note.id === noteId);
 
-  return <div className="item">{note?.body}</div>;
+  return (
+    <>
+      <header>
+        <h3>
+          <Link to="/">
+            <ArrowLeft />
+          </Link>
+        </h3>
+      </header>
+      <textarea value={note?.body}></textarea>
+    </>
+  );
 };
 
 export default NotePage;
